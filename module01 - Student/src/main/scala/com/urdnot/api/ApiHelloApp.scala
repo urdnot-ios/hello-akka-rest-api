240323c2046f024626f3581ad5e0bca8f2f0420f
Jeffrey Sewell <jeffrey@urdnot.com>
2021-10-13 14:00:36 -0700

    updated the student module for module01

--- module01 - Student/src/main/scala/com/urdnot/api/ApiHelloApp.scala
+++ module01 - Student/src/main/scala/com/urdnot/api/ApiHelloApp.scala
@@ -1,27 +1,20 @@
 package com.urdnot.api
 
-import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.Logger
 
-import scala.concurrent.ExecutionContextExecutor
 
-
-object ApiHelloApp extends App {
+object ApiHelloApp extends App with ApiHelloRoutes {
   private val config: Config = ConfigFactory.load()
   private val interface: String = config.getString("akka.server.interface")
   private val httpPort: Int = config.getInt("akka.server.http.port")
   private val log: Logger = Logger("homeApiService")
 
-  // because of the bindingFuture, you need to include the executionContext
-  implicit val system: ActorSystem = ActorSystem()
-  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-
   // setup the routes
-  val route = ApiHelloRoutes.setupRoutes()
+  val route = setupRoutes()
 
-  // bind the interface
+  // bind the interface, load the routes, and catch any errors
   List(Http()
     .newServerAt(interface, httpPort)
     .bind(route)
--- module01 - Student/src/main/scala/com/urdnot/api/ApiHelloRoutes.scala
+++ module01 - Student/src/main/scala/com/urdnot/api/ApiHelloRoutes.scala
@@ -1,27 +1,44 @@
 package com.urdnot.api
 
+import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Directives.{concat, get, path, _}
 import akka.http.scaladsl.server.Route
-import akka.stream.scaladsl.Flow
+import akka.http.scaladsl.server.directives.Credentials
+import akka.stream.IOResult
+import akka.stream.scaladsl.{Flow, Sink}
 import akka.util.ByteString
 import com.typesafe.scalalogging.Logger
 
-object ApiHelloRoutes {
+import java.nio.file.Path
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+trait ApiHelloRoutes {
+
 
   final case class User(userId: String, message: String)
+  private val file: Path = ???
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   private val log: Logger = Logger("routes")
+  val fileSink: Sink[ByteString, Future[IOResult]] = ???
   val appendFlow: Flow[ByteString, ByteString, _] = ???
+
+  def myUserPassAuthenticator(credentials: Credentials): Option[String] = ???
 
   def setupRoutes(): Route = {
     concat(
       path("hello") {
         get {
-          parameters(???) { ??? }
+          parameters( ??? ) { ??? }
         }
       },
       path("helloJson") {
-        post {???}
+        post { ??? }
+      },
+      path("secureRoute") {
+        post( ??? )
       }
     )
   }
